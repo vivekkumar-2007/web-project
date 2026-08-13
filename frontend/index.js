a2ffc108c2717aa91db14b511324cd6e6f859aa5
Vivek <tvivekkumar0987@gmail.com>
2026-08-13 21:00:06 +0530

    new change

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,26 @@
-alert("hello from js")
+// alert("hello from js")
+const users = [
+ {
+   "name" : "Jonn Doe",
+   "gender" : "Male",
+   "image" : "john.png"
+ },
+   {
+   "name" : "Jane Doe",
+   "gender" : "Female",
+   "image" : "jane.png"
+ }
+]
+
+var curIndex = 0;
+
+function toggle(){
+ if(curIndex == 0)
+   curIndex = 1;
+ else
+   curIndex = 0;
+
+ document.getElementById("card-image").src = users[curIndex].image
+ document.getElementById("card-name").innerText =users[curIndex].name
+ document.getElementById("card-gender").innerText=users[curIndex].gender
+}
--- server.js
+++ server.js
@@ -6,3 +6,4 @@ app.use(express.static("frontend"));
 app.listen(port, function () {
   console.log("Server is running on http://localhost:" + port);
 });
+
