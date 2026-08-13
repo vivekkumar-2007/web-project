11406fc0485651d1417b49a5535f4c5d8cbc332a
Vivek <tvivekkumar0987@gmail.com>
2026-08-13 14:49:27 +0530

    running succesful

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static("frontend"));
+app.listen(port, function () {
+  console.log("Server is running on http://localhost:" + port);
+});
